8614d045eca44b0cc0091bc5a9c1bb1a63c9dab4
atomozero <bernardi.andrea@gmail.com>
2026-02-15 21:19:17 +0100

    Bump firmware version to 0.5.2

--- src/main.h
+++ src/main.h
@@ -8,7 +8,7 @@
  * Based on MeshCore protocol: https://github.com/meshcore-dev/MeshCore
  */
 
-#define FIRMWARE_VERSION    "0.5.0"
+#define FIRMWARE_VERSION    "0.5.2"
 
 //=============================================================================
 // Configuration
